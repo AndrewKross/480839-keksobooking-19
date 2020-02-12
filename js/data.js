ca55a1f79166101481ba04942ba7df59c3039f4f
Andrew <andkross@yandex.ru>
2020-02-12 19:37:43 +0300

    убрал окно ошибки в утилс

--- js/data.js
+++ js/data.js
@@ -2,18 +2,6 @@
 // ------------------ модуль создания массивов с данными для объявлений
 (function () {
 
-  var onError = function (errorMessage) {
-    var node = document.createElement('div');
-    node.style = 'z-index: 100; margin: 0 30%; text-align: center; background-color: red;';
-    node.style.position = 'absolute';
-    node.style.left = 0;
-    node.style.right = 0;
-    node.style.fontSize = '24px';
-
-    node.textContent = errorMessage;
-    document.body.insertAdjacentElement('afterbegin', node);
-  };
-
   var onLoad = function (response) {
     window.data.getAdsElement = response;
   };
@@ -21,7 +9,7 @@
   window.data = {
   };
 
-  window.backend.load(onLoad, onError);
+  window.backend.load(onLoad, window.utils.onError);
 
 })();
 
